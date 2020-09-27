7d105acfd1496ff0d891574b4f6a581af1c08359
Christian Ramelow <info@codekandis.net>
2020-09-27 21:37:44 +0200

    Adds the interface `CommonStatelessAuthenticatorInterface`.

---
+++ src/CommonStatelessAuthenticatorInterface.php
@@ -0,0 +1,21 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Authentication;
+
+/**
+ * Represents the interface of all common stateless authenticators.
+ * A common authenticator is based on clients providing an ID and a passcode.
+ * A stateless authenticator does not store the clients permission.
+ * @package codekandis/authentication
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface CommonStatelessAuthenticatorInterface
+{
+	/**
+	 * Requests to grant the client permission.
+	 * @param RegisteredCommonClientInterface[] $registeredClients The registered clients.
+	 * @param CommonClientCredentialsInterface $clientCredentials The credentials of the client requesting permission.
+	 * @return bool True if the client has been granted permission, false otherwise.
+	 * @throws AuthenticationIsCorruptedException The authentication data is corrupted.
+	 */
+	public function requestPermission( array $registeredClients, CommonClientCredentialsInterface $clientCredentials ): bool;
+}
