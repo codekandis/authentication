88276fa2e7a0f49a0d548cb17e13a9f6be83e26c
Christian Ramelow <info@codekandis.net>
2020-09-27 21:35:16 +0200

    Adds the interface `RegisteredCommonClientInterface`.

---
+++ src/RegisteredCommonClientInterface.php
@@ -0,0 +1,34 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Authentication;
+
+/**
+ * Represents the interface of all registered common clients providing an ID and a passcode.
+ * @package codekandis/authentication
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface RegisteredCommonClientInterface
+{
+	/**
+	 * Gets the description of the client.
+	 * @return string The description of the client.
+	 */
+	public function getDescription(): string;
+
+	/**
+	 * Gets the ID of the client.
+	 * @return string The ID of the client.
+	 */
+	public function getId(): string;
+
+	/**
+	 * Gets the passcode of the client.
+	 * @return string The passcode of the client.
+	 */
+	public function getPassCode(): string;
+
+	/**
+	 * Gets the permission of the client.
+	 * @return int The permission of the client.
+	 */
+	public function getPermission(): int;
+}
