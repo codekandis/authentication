77ffb903b49d883286d8534be6087937479c4acd
Christian Ramelow <info@codekandis.net>
2020-09-28 21:40:19 +0200

    Adds the interface `KeyBasedStatelessAuthenticatorInterface`.

---
+++ src/KeyBasedStatelessAuthenticatorInterface.php
@@ -0,0 +1,21 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Authentication;
+
+/**
+ * Represents the interface of all key based stateless authenticators.
+ * A key based authenticator is based on clients providing a key.
+ * A stateless authenticator does not store the clients permission.
+ * @package codekandis/authentication
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface KeyBasedStatelessAuthenticatorInterface
+{
+	/**
+	 * Requests to grant the client permission.
+	 * @param RegisteredKeyBasedClientInterface[] $registeredClients The registered clients.
+	 * @param KeyBasedClientCredentialsInterface $clientCredentials The credentials of the client requesting permission.
+	 * @return bool True if the client has been granted permission, false otherwise.
+	 * @throws AuthenticationIsCorruptedException The authentication data is corrupted.
+	 */
+	public function requestPermission( array $registeredClients, KeyBasedClientCredentialsInterface $clientCredentials ): bool;
+}
