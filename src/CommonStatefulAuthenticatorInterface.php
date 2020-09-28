4a2bfb953f7d5cb17eeb8bbf0cc9b42e6814fc61
Christian Ramelow <info@codekandis.net>
2020-09-28 23:04:39 +0200

    Adds the interface `CommonStatefulAuthenticatorInterface`.

---
+++ src/CommonStatefulAuthenticatorInterface.php
@@ -0,0 +1,24 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Authentication;
+
+/**
+ * Represents the interface of all common stateful authenticators.
+ * A common authenticator is based on clients providing an ID and a passcode.
+ * A stateful authenticator stores the clients permission.
+ * @package codekandis/authentication
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface CommonStatefulAuthenticatorInterface extends CommonStatelessAuthenticatorInterface
+{
+	/**
+	 * Determines if the client has been granted permission.
+	 * @return bool True if the client has been granted permission, false otherwise.
+	 * @throws AuthenticationIsCorruptedException The authentication data is corrupted.
+	 */
+	public function isClientGranted(): bool;
+
+	/**
+	 * Revokes the permission of the client.
+	 */
+	public function revokePermission(): void;
+}
