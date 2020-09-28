2c59d5c2fabf2a2028426e8701532427482ce362
Christian Ramelow <info@codekandis.net>
2020-09-28 23:23:55 +0200

    Adds the interface `KeyBasedStatefulAuthenticatorInterface`.

---
+++ src/KeyBasedStatefulAuthenticatorInterface.php
@@ -0,0 +1,24 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Authentication;
+
+/**
+ * Represents the interface of all key based stateful authenticators.
+ * A key based authenticator is based on clients providing a key.
+ * A stateful authenticator stores the clients permission.
+ * @package codekandis/authentication
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface KeyBasedStatefulAuthenticatorInterface extends KeyBasedStatelessAuthenticatorInterface
+{
+	/**
+	 * Determines if the client has been granted permission.
+	 * @return bool True if the client has been granted permission, false otherwise.
+	 * @throws AuthenticationIsCorruptedException The authentication data is corrupted.
+	 */
+	public function isClientGranted(): bool;
+
+	/**
+	 * Revokes the permission of the client.
+	 */
+	public function revokePermission(): void;
+}
