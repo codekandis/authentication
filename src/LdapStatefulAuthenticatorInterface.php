43239787b8c1c4ffe1bccca2f5b1a65586b1f10d
Christian Ramelow <info@codekandis.net>
2022-05-25 21:09:18 +0200

    Adds a LDAP stateful authenticator interface.

---
+++ src/LdapStatefulAuthenticatorInterface.php
@@ -0,0 +1,24 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Authentication;
+
+/**
+ * Represents the interface of all LDAP stateful authenticators.
+ * A LDAP authenticator is based on clients providing a key.
+ * A stateful authenticator stores the clients' permission.
+ * @package codekandis/authentication
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface LdapStatefulAuthenticatorInterface extends LdapStatelessAuthenticatorInterface
+{
+	/**
+	 * Determines if the client has been granted permission.
+	 * @return bool True if the client has been granted permission, false otherwise.
+	 * @throws AuthenticationIsCorruptedException The authentication data is corrupted.
+	 */
+	public function isClientGranted(): bool;
+
+	/**
+	 * Revokes the permission of the client.
+	 */
+	public function revokePermission(): void;
+}
